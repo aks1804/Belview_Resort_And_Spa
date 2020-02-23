45e03a7be9c273fcf2263cc46bc011d0550532d0
Mohammed Rushad <mohammedrushad.181co232@gmail.com>
2020-02-23 16:55:33 +0530

    test changes

--- Belview_Resort_And_Spa/settings.py
+++ Belview_Resort_And_Spa/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Belview_Resort_And_Spa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'PRACTICE1',
+        'USER': 'root',
+        'PASSWORD': 'bCA3DB@99A2',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
